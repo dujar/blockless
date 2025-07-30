9005ef0ac8708c36d7316a447a4b8befcb0520fb
fab <dujar.coding@gmail.com>
2025-07-30 16:41:43 +0700

    export commonjs

--- api/proxy/index.ts
+++ api/proxy/index.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+module.exports = async function handler(req: VercelRequest, res: VercelResponse) {
 
   // Allow only http://localhost:* or a single user-defined origin
   const origin = req.headers.origin || '';
